5f57b6af993b13d1c05a68ccca4b008cfb9b960c
Matthew <matteidsen@gmail.com>
2024-06-17 21:12:57 -0700

    initial commit

--- server/server.js
+++ server/server.js
@@ -2,8 +2,13 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+
+ app.get("/", (req, res) => {
+    res.send("Hello world!");
+ });
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`server is up and listening on port ${port}`);
-})
+});
