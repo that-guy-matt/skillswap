612e02ccf657b0ed8f5b64622a3770fd8c634218
Matthew <matteidsen@gmail.com>
2024-07-08 13:06:26 -0700

    add signup and login endpoints and functionality

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const path = require("path");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 const {PrismaClient} = require("@prisma/client");
 
 dotenv.config({ path: path.resolve(__dirname, '../.env')});
@@ -8,6 +10,8 @@ dotenv.config({ path: path.resolve(__dirname, '../.env')});
 const prisma = new PrismaClient();
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
@@ -21,6 +25,67 @@ app.get("/users", async (req, res) => {
         console.error("Error fetching users", err);
         res.status(500).json({ error: "Internal Server Error" });
       }
+});
+
+
+app.post("/signup", async (req, res) => {
+
+  const {email, password} = req.body;
+  try {
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await prisma.user.create({
+
+      data: {
+        email: email,
+        password: hashedPassword
+      }
+
+    });
+
+    console.log({ email: email, password: hashedPassword });
+    res.status(201).json(newUser);
+
+  } catch (e) {
+
+    console.error("Error creating user", e);
+    res.status(500).json({error: "Internal server error"});
+
+  }
+  
+});
+
+app.post("/login", async (req, res) => {
+
+  const {email, password} = req.body;
+  try {
+
+    const user = await prisma.user.findUnique({
+      where: { email: email }
+    });
+
+    if (!user) {
+
+      return res.status(404).json({error: "User not found"});
+
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+
+      res.status(200).json({error: "Invalid credentials"});
+
+    }
+
+    res.status(200).json({message: "Login successful"});
+
+  } catch (e) {
+
+    console.error("Error logging in", e);
+    res.status(500).json({error: "Internal server error"});
+
+  }
+
 });
 
 const port = process.env.PORT || 3000;
